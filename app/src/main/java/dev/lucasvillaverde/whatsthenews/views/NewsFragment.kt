bb287422f9024e4f4b18e427e69672ba8351e929
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-05-26 23:39:28 -0300

    Fragment and activity main xml modified, inserted frame layout

--- app/src/main/java/dev/lucasvillaverde/whatsthenews/views/NewsFragment.kt
+++ app/src/main/java/dev/lucasvillaverde/whatsthenews/views/NewsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.whatsthenews.R
+import dev.lucasvillaverde.whatsthenews.adapters.NewsAdapter
 import dev.lucasvillaverde.whatsthenews.utils.inflate
 import kotlinx.android.synthetic.main.fragment_news.*
 
@@ -18,10 +19,20 @@ class NewsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = container?.inflate(R.layout.fragment_news)
+        return container?.inflate(R.layout.fragment_news)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         newsList.setHasFixedSize(true)
         newsList.layoutManager = LinearLayoutManager(context)
 
-        return view
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        if (newsList.adapter == null){
+            newsList.adapter = NewsAdapter()
+        }
     }
 }
